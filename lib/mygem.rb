9e11f0b26fcd435bd63db3b70525827bfba8e8b4
sree5136 <sree5136@gmail.com>
2017-01-18 14:56:47 +0530

    Update mygem.rb

--- lib/mygem.rb
+++ lib/mygem.rb
@@ -1,8 +1,8 @@
 require "mygem/version"
 
 module Mygem
-  class self.TestClass
-      def notify
+  class TestClass
+      def self.notify
           puts('you have included mygem in your project good luck!!!')
       end
   end
